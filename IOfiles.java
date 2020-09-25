0b4e83dccf8691bb578d0eccdd3097d039261691
Adigoon <Alex.opakunle@gmail.com>
2020-09-25 23:03:42 +0100

    Newer diskWriter files
    IoFiles2 is the fixed version

---
+++ IOfiles.java
@@ -0,0 +1,26 @@
+package yabo;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
+public class IOfiles {
+
+	public static void main(String[] args)throws FileNotFoundException {
+			Scanner s = new Scanner(new File("raawData.txt"));
+			PrintStream p = new PrintStream("cookedData.txt");
+			double unitPrice, total;
+			int quantity;
+			
+			unitPrice = s.nextDouble();
+			quantity = s.nextInt();
+			
+			total = unitPrice * quantity;
+			
+			p.println(total);
+			
+			p.close();
+			s.close();
+	}
+
+}
---
+++ IoFiles2.java
@@ -0,0 +1,10 @@
+package yabo;
+
+public class IoFiles2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
