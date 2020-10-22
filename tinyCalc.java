d2587ca4c4ce540616de632716f4cd60275679a7
Adigoon <Alex.opakunle@gmail.com>
2020-10-22 22:13:51 +0100

    Update tinyCalc.java

--- tinyCalc.java
+++ tinyCalc.java
@@ -71,9 +71,9 @@ public class tinyCalc {
 			out.println(num1 / num2);
 			break;
 		}
-		// This might be the statement i 
-		// use to build the GUI behind my first calculator
-		// shoudlbe ready next month
+		// Built a basic calculator using a switch statement 
+		
+	
 	}
 
 }
