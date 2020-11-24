0d2079d7dee2e7b64ec84fa51559123c81d1d1cd
Olaotan Ajibade Opakunle <Alex.opakunle@gmail.com>
2020-11-24 20:20:52 +0100

    Update IOfiles.java

--- IOfiles.java
+++ IOfiles.java
@@ -9,7 +9,7 @@ public class IOfiles {
 	public static void main(String[] args)throws FileNotFoundException {
 			Scanner s = new Scanner(new File("raawData.txt"));
 			PrintStream p = new PrintStream("cookedData.txt");
-			double unitPrice, total;
+			double unitPrice, total; 
 			int quantity;
 			
 			unitPrice = s.nextDouble();
